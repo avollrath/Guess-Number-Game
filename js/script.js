8b522ad5b38c40dd12d556332ee4e521acc26a35
André Vollrath <andrevollrath@Andres-MacBook-Pro.local>
2019-02-27 20:23:17 +0200

    added mute button

--- js/script.js
+++ js/script.js
@@ -4,9 +4,11 @@ let randomNumber = Math.floor(Math.random() * 100) + 1;
 const lastResult = document.querySelector('.lastResult');
 const guessSubmit = document.querySelector('.guessSubmit');
 const guessField = document.querySelector('.guessField');
+const muteButton = document.querySelector('.mute');
 const tries = document.querySelector('.tries');
 let guessCount = 1;
 let resetButton;
+var mute = false;
 var themeMusic;
 var wrongSound;
 var winnerSound;
@@ -25,6 +27,7 @@ function sound(src) {
     this.sound.src = src;
     this.sound.setAttribute("preload", "auto");
     this.sound.setAttribute("controls", "none");
+    this.sound.setAttribute("controls", "none");
     this.sound.style.display = "none";
     document.body.appendChild(this.sound);
     this.play = function(){
@@ -35,7 +38,36 @@ function sound(src) {
     }
   }
 
+
+ 
+
+  
+themeMusic.sound.addEventListener('ended', function() {
+    this.currentTime = 0;
+    this.play();
+    }, 
+    false);  
+
+
+function muteOnOff(){
+
+if (mute === false ){
+        themeMusic.stop();
+        mute = true;
+        }
+else {
+        themeMusic.play();
+        mute = false;
+        }
+
+}
+
+
+
+
 themeMusic.play();
+guessSubmit.addEventListener('click', checkGuess);
+muteButton.addEventListener('click', muteOnOff);
 
 
 
@@ -50,23 +82,23 @@ function checkGuess() {
     if (userGuess === randomNumber) {
       lastResult.textContent = 'Wow! You got it right, lucky bastard!';
       themeMusic.stop();
-      winnerSound.play();
+      if (mute === false) winnerSound.play();
       setGameOver();
     } else if (guessCount === 10) {
       lastResult.textContent = '###!!! GAME OVER LOSER !!!###';
       themeMusic.stop();
-      loserSound.play();
+      if (mute === false) loseSound.play();
       setGameOver();
     } else {
       
       
       if(userGuess < randomNumber) {
-        wrongSound.play();
+        if (mute === false) wrongSound.play();
         lastResult.textContent = 'Too low, idiot!';
         guessSubmit.value = 'Check again';
 
       } else if(userGuess > randomNumber) {
-        wrongSound.play();
+        if (mute === false) wrongSound.play();
         lastResult.textContent = 'Jerk, that is too high!';
         guessSubmit.value = 'Check again';
 
@@ -79,7 +111,7 @@ function checkGuess() {
   }
 
 
-  guessSubmit.addEventListener('click', checkGuess);
+
 
 
   function setGameOver() {
@@ -102,7 +134,7 @@ function checkGuess() {
     tries.appendChild(lives);
     }    
 
-    themeMusic.play();
+    if (mute === false) themeMusic.play();
     lastResult.textContent = 'Please guess a number:';
     guessCount = 1;
     guessSubmit.value = 'Check';
